5cbdb8572ecc48f53efc2914873d3dc923234e88
Miyacta <s.miya211@gmail.com>
2023-03-09 08:23:29 +0900

    ブロックリストページ作成

---
+++ src/components/pages/home/block.tsx
@@ -0,0 +1,278 @@
+import { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FirebaseError } from 'firebase/app';
+import { useAtom } from 'jotai';
+
+import styles from './block.module.css';
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import Button from '@/components/atoms/Button';
+import Input from '@/components/atoms/Input';
+import Skeleton from '@/components/atoms/Skeleton';
+import Modal from '@/components/molecules/Modal';
+import AvatarList from '@/components/organisms/AvatarList';
+import Header from '@/components/organisms/Header';
+import PcNavigation from '@/components/organisms/PcNavigation';
+
+import { useSearch, useBlock } from '@/features';
+import { useFriend } from '@/hooks';
+import { authUserAtom, friendsIdAtom } from '@/store';
+import { getFirebaseError, deleteBlockUser, setFriend } from '@/utils';
+
+const Block = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [authUser] = useAtom(authUserAtom);
+  const [friends] = useAtom(friendsIdAtom);
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(
+    '予期せぬエラーが発生しました。お手数ですが、再度ログインしてください。'
+  );
+  const [isOpenErrorModal, setIsOpenErrorModal] = useState(false);
+  const [isOpenCompleteModal, setIsOpenCompleteModal] = useState(false);
+
+  const {
+    getBlockUserIdList,
+    saveBlockUserIdList,
+    setBlockUserList,
+    blockUserList,
+    saveBlockUserData,
+    getSearchedBlockUser,
+  } = useBlock();
+
+  const { saveFriendIdList } = useFriend();
+  const { searchUserList } = useSearch();
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const onExcludeUser = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    index: number
+  ) => {
+    if (!userId) return;
+
+    const deleteItemsList = [...blockUserList];
+    const unBlockUserId = deleteItemsList[index];
+
+    deleteItemsList.splice(index, 1);
+
+    await deleteBlockUser(userId, unBlockUserId);
+
+    await setFriend(userId, unBlockUserId);
+
+    saveBlockUserIdList(deleteItemsList);
+    setBlockUserList(deleteItemsList);
+
+    const friendIdListCache = friends?.data as string[];
+    friendIdListCache.push(unBlockUserId);
+    saveFriendIdList(friendIdListCache);
+
+    setIsOpenCompleteModal(true);
+  };
+
+  const userId = authUser?.uid || '';
+  useEffect(() => {
+    try {
+      if (!userId) return;
+      getBlockUserIdList(true).then((blockUserIdList) => {
+        saveBlockUserIdList(blockUserIdList);
+        saveBlockUserData(blockUserIdList);
+      });
+      setIsLoading(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        setErrorMessage(getFirebaseError(errorCode));
+      }
+
+      setIsOpenErrorModal(true);
+    }
+  }, [userId]);
+
+  const searchFriend = async () => {
+    const searchList = await searchUserList(search);
+    const searchedBlockUserIdList = await getSearchedBlockUser(
+      searchList,
+      userId
+    );
+    if (searchedBlockUserIdList) {
+      setBlockUserList(searchedBlockUserIdList);
+    } else {
+      setBlockUserList([]);
+    }
+  };
+
+  useEffect(() => {
+    if (!userId) return;
+
+    try {
+      if (!search) {
+        getBlockUserIdList(true).then((blockUserIdList) => {
+          saveBlockUserIdList(blockUserIdList);
+          saveBlockUserData(blockUserIdList);
+        });
+      }
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        setErrorMessage(getFirebaseError(errorCode));
+      } else if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+      setIsOpenErrorModal(true);
+    }
+  }, [search]);
+
+  const scrollRefCurrent = scrollRef.current;
+  const isPcWindow = window.matchMedia('(min-width:1024px)').matches;
+
+  const handleScroll = async () => {
+    if (!scrollRefCurrent) return;
+
+    let contentsHeight;
+    if (isPcWindow) {
+      contentsHeight = window.innerHeight - 80;
+    } else {
+      contentsHeight = window.innerHeight - 136;
+    }
+
+    if (
+      scrollRefCurrent.scrollHeight !==
+      scrollRefCurrent.scrollTop + contentsHeight
+    )
+      return;
+
+    getBlockUserIdList(false).then((blockUserIdList) => {
+      saveBlockUserIdList(blockUserIdList);
+      saveBlockUserData(blockUserIdList);
+    });
+  };
+  useEffect(() => {
+    scrollRefCurrent?.addEventListener('scroll', handleScroll);
+
+    return () => {
+      scrollRefCurrent?.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollRefCurrent]);
+
+  const renderCompleteModal = () => {
+    if (!isOpenCompleteModal) return;
+
+    return (
+      <Modal
+        onClose={() => setIsOpenCompleteModal(false)}
+        title="完了"
+        titleAlign="center"
+        hasInner
+        isOpen={isOpenCompleteModal}
+        isBoldTitle
+      >
+        <div>
+          <p>ブロックを解除しました</p>
+        </div>
+        <div className={styles.controler}>
+          <Button
+            color="primary"
+            onClick={() => navigate('/')}
+            variant="contained"
+            isFullWidth
+            size="small"
+          >
+            OK
+          </Button>
+        </div>
+      </Modal>
+    );
+  };
+
+  const renderErrorModal = () => {
+    if (!isOpenErrorModal) return;
+
+    return (
+      <Modal
+        onClose={() => setIsOpenErrorModal(false)}
+        title="エラー"
+        titleAlign="center"
+        hasInner
+        isOpen={isOpenErrorModal}
+        isBoldTitle
+      >
+        <div>
+          <p>{errorMessage}</p>
+        </div>
+        <div className={styles.controler}>
+          <Button
+            color="primary"
+            onClick={() => setIsOpenErrorModal(false)}
+            variant="contained"
+            isFullWidth
+            size="small"
+          >
+            OK
+          </Button>
+        </div>
+      </Modal>
+    );
+  };
+
+  return (
+    <>
+      {renderErrorModal()}
+      {renderCompleteModal()}
+      <Header title="ブロックユーザー" className="sp" showBackButton />
+      <main className={`${styles.pcLayout} grow`}>
+        {isLoading && (
+          <div className={styles.container}>
+            <div className={styles.tabs}>
+              <Skeleton variant="rectangular" height={32} />
+            </div>
+            <div className={`${styles.searchForm} inner`}>
+              <Skeleton variant="rectangular" height={32} />
+            </div>
+            <div className={styles.contents}>
+              <Skeleton variant="rectangular" height={550} />
+              <div className={styles.fab}>
+                <Skeleton variant="circular" />
+              </div>
+            </div>
+          </div>
+        )}
+        {!isLoading && (
+          <div className={styles.container}>
+            <div className={`${styles.searchForm} inner flex`}>
+              <Input
+                color="primary"
+                id="search"
+                onChange={(event) => setSearch(event.target.value)}
+                type="text"
+                value={search}
+                variant="filled"
+                isFullWidth
+                placeholder="search"
+                startIcon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+              />
+              <button onClick={searchFriend}>検索</button>
+            </div>
+            <div ref={scrollRef} className={styles.contents}>
+              <AvatarList
+                idList={blockUserList}
+                isBlockUser
+                onClick={onExcludeUser}
+                showDeleteButton
+              />
+            </div>
+          </div>
+        )}
+        <div className="pc">
+          <PcNavigation>解除ボタンでブロック解除できます</PcNavigation>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Block;
---
+++ src/features/useBlock.ts
@@ -0,0 +1,146 @@
+import { useState, useRef } from 'react';
+
+import { User } from 'firebase/auth';
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  startAfter,
+  DocumentData,
+  QueryDocumentSnapshot,
+  QueryConstraint,
+  where,
+  documentId,
+} from 'firebase/firestore';
+import { useAtom } from 'jotai';
+
+import { INITIAL_ICON_URL } from '@/constants';
+import { useUser } from '@/hooks';
+import { db } from '@/main';
+import { authUserAtom, blockUserIdAtom } from '@/store';
+import { getCacheExpirationDate, isCacheActive } from '@/utils';
+
+export const useBlock = () => {
+  const [authUser] = useAtom(authUserAtom);
+  const [blockUser, setBlockUser] = useAtom(blockUserIdAtom);
+  const [blockUserList, setBlockUserList] = useState<string[]>([]);
+  const [lastBlockUser, setLastBlockUser] =
+    useState<QueryDocumentSnapshot<DocumentData>>();
+  const { getUser, saveUser } = useUser();
+
+  const userRef = useRef<User>();
+  const lastBlockUserRef = useRef<QueryDocumentSnapshot<DocumentData>>();
+  const blockUserListRef = useRef<string[]>();
+
+  if (authUser) userRef.current = authUser;
+  if (lastBlockUserRef) lastBlockUserRef.current = lastBlockUser;
+  if (blockUserList) blockUserListRef.current = blockUserList;
+
+  const getBlockUserIdList = async (isUsedCache: boolean) => {
+    // この関数はaddEventlistener内で呼ばれるため、
+    // 最新のstateを参照するべくrefから取得する
+    const userId = userRef.current?.uid;
+    const lastUnderBlockUser = lastBlockUserRef.current;
+    const blockUserNewIdList = blockUserListRef.current;
+
+    if (isUsedCache && blockUser && isCacheActive(blockUser)) {
+      const blockUserCacheIdList = blockUser?.data as string[];
+      setBlockUserList(blockUserCacheIdList);
+
+      return blockUserCacheIdList;
+    }
+
+    if (!userId) throw new Error('ユーザー情報がありません。');
+
+    const groupRef = collection(db, 'users', userId, 'blockUser');
+    const queryArray: QueryConstraint[] = [
+      orderBy('updatedAt', 'desc'),
+      limit(10),
+    ];
+
+    if (lastUnderBlockUser) queryArray.push(startAfter(lastUnderBlockUser));
+
+    const querySnapshots = await getDocs(query(groupRef, ...queryArray));
+    const lastVisible = querySnapshots.docs[querySnapshots.docs.length - 1];
+    if (lastVisible) setLastBlockUser(lastVisible);
+
+    const blockUserIdList: string[] = [];
+    for (const doc of querySnapshots.docs) {
+      const groupId = doc.id;
+      blockUserIdList.push(groupId);
+    }
+
+    if (!blockUserNewIdList) {
+      setBlockUserList(blockUserIdList);
+
+      return blockUserIdList;
+    }
+
+    const newList = blockUserNewIdList.concat(blockUserIdList);
+    setBlockUserList(newList);
+
+    return newList;
+  };
+
+  // 検索したグループ一覧をグローバルステートに保存して、一覧を返す
+  const saveBlockUserData = async (blockUserIdList: string[]) => {
+    if (!blockUserIdList || blockUserIdList.length === 0) return;
+
+    for (const blockUserId of blockUserIdList) {
+      let blockUserData = await getUser(blockUserId);
+
+      if (!blockUserData) {
+        const now = new Date();
+        const deletedUserData = {
+          name: '退会済みユーザー',
+          iconUrl: INITIAL_ICON_URL,
+          createdAt: now,
+          updatedAt: now,
+        };
+
+        blockUserData = deletedUserData;
+      }
+      saveUser(blockUserId, blockUserData);
+    }
+  };
+  /**
+   * 名前検索したユーザーリストを受け取り、追加済みフレンドを返す
+   */
+  const getSearchedBlockUser = async (searchList: string[], userId: string) => {
+    const searchedBlockUserIdList: string[] = [];
+    if (searchList.length === 0) return;
+
+    const usersRef = collection(db, 'users', userId, 'blockUser');
+    const snapshot = await getDocs(
+      query(usersRef, where(documentId(), 'in', searchList))
+    );
+
+    for (const doc of snapshot.docs) {
+      const searchId = doc.id;
+      searchedBlockUserIdList.push(searchId);
+    }
+
+    return searchedBlockUserIdList;
+  };
+
+  /**
+   * グローバルstateの情報を更新
+   */
+  const saveBlockUserIdList = (blockUserIdList: string[]) => {
+    setBlockUser({
+      data: blockUserIdList,
+      expiresIn: getCacheExpirationDate(),
+    });
+  };
+
+  return {
+    getBlockUserIdList,
+    saveBlockUserIdList,
+    setBlockUserList,
+    blockUserList,
+    saveBlockUserData,
+    getSearchedBlockUser,
+  };
+};
