056f8b1aae140b5da47f2e80923cb29c48f74f94
Miyacta <s.miya211@gmail.com>
2023-03-09 06:35:27 +0900

    テストのリファクタリング実施

--- tests/rules/room.test.ts
+++ tests/rules/room.test.ts
@@ -8,7 +8,6 @@ import {
 import {
   setDoc,
   getDoc,
-  getDocs,
   updateDoc,
   deleteDoc,
   doc,
@@ -19,14 +18,7 @@ import {
 } from '@firebase/firestore';
 
 import { readFileSync } from 'fs';
-import {
-  afterAll,
-  afterEach,
-  beforeAll,
-  beforeEach,
-  describe,
-  it,
-} from 'vitest';
+import { afterAll, afterEach, beforeAll, describe, it } from 'vitest';
 import { v4 as uuidv4 } from 'uuid';
 
 let testEnv: RulesTestEnvironment;
